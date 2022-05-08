d45ed3acc599ee53356a7f640eba8e83d92fefab
Mikhail <mudrogelov.beled@gmail.com>
2022-05-08 17:49:19 +0300

    set project

---
+++ src/components/App/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../../style.scss';
+
+class App extends React.Component {
+    render() {
+        return (
+            <>
+                <div>
+                    Bridge
+                </div>
+            </>
+        )
+    }
+}
+
+export default App;
